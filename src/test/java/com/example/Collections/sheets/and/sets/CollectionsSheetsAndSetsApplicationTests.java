51dc3f83bc0d3f37bf331f0af1f46d60f478c622
origin <kruglov56832@mail.ru>
2024-07-01 13:42:49 +0900

    Collections sheets and sets_1

---
+++ src/test/java/com/example/Collections/sheets/and/sets/CollectionsSheetsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Collections.sheets.and.sets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsSheetsAndSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
